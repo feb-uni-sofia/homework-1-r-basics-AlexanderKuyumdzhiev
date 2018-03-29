f5c663316a091e36561ee23c74b92578d16497cc
Aleksandar Kuyumdzhiev <akuyumdzhiev@yahoo.com>
2018-03-29 20:02:21 +0300

    Done

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,17 @@
-
+#a
+x <- c(4, 1, 1, 4)
+#b
+y <- c(1,4)
+#c
+x-y #The difference between two vectors
+#d
+s <- c(x, y)
+#e
+rep(s, 10)
+length(rep(s, 10))
+#f
+rep(s, each=3)
+#g
+seq(from=7, to=21)
+7:21
+length(7:21)
